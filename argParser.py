d5ed426749eaf523f4a99dace2cc772dec9699d5
Vahid_Jani <62806751+vahid8@users.noreply.github.com>
2021-05-18 09:36:41 +0200

    Create argParser

---
+++ argParser.py
@@ -0,0 +1,18 @@
+import argparse #import package
+# functions definition
+def get_user_input_arguments():
+    """ To get user input values for ip address , port """
+
+    my_parser = argparse.ArgumentParser()
+
+    my_parser.add_argument('--port', action='store', type=int, default="5566")
+    my_parser.add_argument('--ip', action='store', type=str,default="0.0.0.0")
+
+    args = my_parser.parse_args()
+
+    return args.ip, int(args.port)
+
+
+if __name__ == '__main__':
+
+    ip, port = get_user_input_arguments()
